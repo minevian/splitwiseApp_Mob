020d39f964e12c2fd0b9423319328ab06c9d8852
miracline2 <francesca4518@gmail.com>
2025-03-21 22:15:11 +0530

    Commented Local Host BaseUrl

--- app/services/MultimediaClientApi.ts
+++ app/services/MultimediaClientApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiMultimediaClient = axios.create({
-    baseURL:'http://localhost:8080/api/',
-    // baseURL:'https://splitwiseapp-api.onrender.com/api',
+    // baseURL:'http://localhost:8080/api/',
+    baseURL:'https://splitwiseapp-api.onrender.com/api',
     headers:{
        'Content-Type': 'multipart/form-data' 
     }
